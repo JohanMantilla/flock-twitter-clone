57e21fd1c34c0f275f2ad6cc6556b69bf7e31b29
Johan <johan.mantilla01@epn.edu.ec>
2026-06-07 19:50:18 -0500

    chore: configure PostgreSQL and TypeORM connection

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,12 +1,25 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { configuration } from './config/configuration';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       load: [configuration],
-    })
+    }),
+
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT!,
+      database: process.env.DB_NAME,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      autoLoadEntities: true,
+      synchronize: true
+    }),
+
   ],
   controllers: [],
   providers: [],
