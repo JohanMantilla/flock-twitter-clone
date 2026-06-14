9d48f32941a121a3bfb7a368b28dc9e6202c472a
Johan <johan.mantilla01@epn.edu.ec>
2026-06-13 21:13:13 -0500

    test(setup): add MSW lifecycle and vitest mock cleanup configuration

--- frontend/src/test/setup.ts
+++ frontend/src/test/setup.ts
@@ -1,7 +1,12 @@
 import '@testing-library/jest-dom';
-import { beforeAll, afterAll, afterEach } from 'vitest';
+import { beforeAll, afterAll, afterEach, vi } from 'vitest';
 import { server } from './mocks/server';
 
 beforeAll(() => server.listen({ onUnhandledRequest: 'warn' }));
-afterEach(() => server.resetHandlers());
+
+afterEach(() => {
+    server.resetHandlers();
+    vi.clearAllMocks();
+});
+
 afterAll(() => server.close());
